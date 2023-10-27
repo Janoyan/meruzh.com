b88716b751399d339b99c387b2bbbcc586427607
Meruzh Janoyan <meruzh2008@gmail.com>
2023-10-27 10:26:42 +0400

    main page

--- script.js
+++ script.js
@@ -1,6 +1,49 @@
 var menuOpen = false;
+window.alwaysOpen = false;
 
 var menuItems = [
+  {
+    title: 'Six tips to speed up Node.js Apps development',
+    description: 'This article highlights the six most important tips I have learned by analyzing my previous projects from the last few years. In it, I have compiled a list of recommendations to help in facilitating the development process of node.js apps. There aren\'t only six tips, of course, I\'ve only listed the six most important ones according to my experience.',
+    image: '/images/node-js.jpg',
+    link: '/articles/6-tips-to-speed-up-node-js-apps-dev',
+    dateString: 'Dec 16, 2020'
+  },
+  {
+    title: 'Six tips to speed up Node.js Apps development',
+    description: 'This article highlights the six most important tips I have learned by analyzing my previous projects from the last few years. In it, I have compiled a list of recommendations to help in facilitating the development process of node.js apps. There aren\'t only six tips, of course, I\'ve only listed the six most important ones according to my experience.',
+    image: '/images/node-js.jpg',
+    link: '/articles/6-tips-to-speed-up-node-js-apps-dev',
+    dateString: 'Dec 16, 2020'
+  },
+  {
+    title: 'Six tips to speed up Node.js Apps development',
+    description: 'This article highlight years. In important ones according to my experience.',
+    image: '/images/node-js.jpg',
+    link: '/articles/6-tips-to-speed-up-node-js-apps-dev',
+    dateString: 'Dec 16, 2020'
+  },
+  {
+    title: 'Six tips to speed up Node.js Apps development',
+    description: 'This article highlights the six most important tips I have learned by analyzing my previous projects from tights the six most important tips I have learned by analyzing my previous projects from tights the six most important tips I have learned by analyzing my previous projects from tights the six most important tips I have learned by analyzing my previous projects from tights the six most important tips I have learned by analyzing my previous projects from tights the six most important tips I have learned by analyzing my previous projects from the last few years. In it, I have compiled a list of recommendations to help in facilitating the development process of node.js apps. There aren\'t only six tips, of course, I\'ve only listed the six most important ones according to my experience.',
+    image: '/images/node-js.jpg',
+    link: '/articles/6-tips-to-speed-up-node-js-apps-dev',
+    dateString: 'Dec 16, 2020'
+  },
+  {
+    title: 'Six tips to speed up Node.js Apps development',
+    description: 'This article highlights the six most important tips I have learned by analyzing my previous projects from the last few years. In it, I have compiled a list of recommendations to help in facilitating the development process of node.js apps. There aren\'t only six tips, of course, I\'ve only listed the six most important ones according to my experience.',
+    image: '/images/node-js.jpg',
+    link: '/articles/6-tips-to-speed-up-node-js-apps-dev',
+    dateString: 'Dec 16, 2020'
+  },
+  {
+    title: 'Six tips to speed up Node.js Apps development',
+    description: 'This article highlights the six most important tips I have learned by analyzing my previous projects from the last few years. In it, I have compiled a list of recommendations to help in facilitating the development process of node.js apps. There aren\'t only six tips, of course, I\'ve only listed the six most important ones according to my experience.',
+    image: '/images/node-js.jpg',
+    link: '/articles/6-tips-to-speed-up-node-js-apps-dev',
+    dateString: 'Dec 16, 2020'
+  },
   {
     title: 'Six tips to speed up Node.js Apps development',
     description: 'This article highlights the six most important tips I have learned by analyzing my previous projects from the last few years. In it, I have compiled a list of recommendations to help in facilitating the development process of node.js apps. There aren\'t only six tips, of course, I\'ve only listed the six most important ones according to my experience.',
@@ -12,7 +55,7 @@ var menuItems = [
 
 document.addEventListener("DOMContentLoaded", () => {
   document.body.innerHTML = `
- <div class="liqued-button">
+ <div id="liqued-button">
   <section class="stage">
         <figure class="ball bubble" id="menu-button">
             <img id="jan-button" src="/images/jan.jpeg">
@@ -36,21 +79,36 @@ document.addEventListener("DOMContentLoaded", () => {
   ${document.body.innerHTML}`;
 
   document.querySelector('#menu-button').addEventListener('click', () => {
-    if (menuOpen) {
-      document.getElementById('menu-container').classList.remove('active');
-      document.getElementById('jan-button').classList.remove('active');
-      document.getElementById('container').classList.add('active');
-      menuOpen = false;
+    if (menuOpen && !window.alwaysOpen) {
+      closeMenu();
     } else {
-      document.getElementById('menu-container').classList.add('active');
-      document.getElementById('jan-button').classList.add('active');
-      document.getElementById('container').classList.remove('active');
-      menuOpen = true;
+      openMenu();
     }
   });
+
+  console.log(window.alwaysOpen, '=====')
+  if (window.alwaysOpen) {
+    openMenu();
+    document.getElementById('liqued-button').classList.add('hidden');
+  }
 });
 
 function navigateTo(link) {
   location.href = link;
+}
+
+function openMenu() {
+  document.getElementById('menu-container').classList.add('active');
+  document.getElementById('jan-button').classList.add('active');
+  document.getElementById('container').classList.remove('active');
+  menuOpen = true;
+}
+
+
+function closeMenu() {
+  document.getElementById('menu-container').classList.remove('active');
+  document.getElementById('jan-button').classList.remove('active');
+  document.getElementById('container').classList.add('active');
+  menuOpen = false;
 }
 
